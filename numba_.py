2a181e9addfb01860472ed03d3320673b2378d21
Denis Anikin <ad@xfenix.ru>
2023-02-04 10:55:17 +0500

    And numba

---
+++ numba_.py
@@ -0,0 +1,14 @@
+import _base
+import numba
+
+
+@numba.jit
+def run_example():
+    output_buf = []
+    for one_number in range(100):
+        output_buf.append(one_number * one_number)
+    return output_buf
+
+
+if __name__ == "__main__":
+    _base.run_timeit()
